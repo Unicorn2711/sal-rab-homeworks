aa241e1de1022dcdcefef51c7ceb682b1ba72842
Valeriia <leram95@yandex.ru>
2023-08-28 12:38:11 +0300

    task4

--- task_4/solution.js
+++ task_4/solution.js
@@ -13,7 +13,7 @@ return products
 function renderProductsCards(json) {
     clearProducts();
     let products = parseProducts(json);
-    length = products.length;
+    let length = products.length;
     for (let i = 0; i < length; i += 1) {
         // место для пункта 7
         addProduct(products[i]);
